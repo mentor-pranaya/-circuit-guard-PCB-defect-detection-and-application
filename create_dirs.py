fdd52f15cab226ab4592dc8d5cc31d45010e6a22
nousheen2309 <md.nousheen7867@gmail.com>
2025-09-02 19:08:38 +0530

    Delete create_dirs.py

--- create_dirs.py
+++
@@ -1,5 +0,0 @@
-# create_dirs.py
-import os
-os.makedirs('outputs_module2/bbox', exist_ok=True)
-os.makedirs('outputs_module2/rois/unlabeled', exist_ok=True)
-print("created outputs_module2/bbox and outputs_module2/rois/unlabeled")
